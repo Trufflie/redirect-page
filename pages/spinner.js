4413780c7fab58d0378f4ecd0433977315b75f26
chris fa <truffliegg@gmail.com>
2024-10-01 21:01:22 -0700

    initial commit, still need to fix layout, but i'm tired

---
+++ pages/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { RingLoader } from 'react-spinners'
+function spinner() {
+  return (
+    <div>
+        <RingLoader 
+        color='aqua'
+        />
+    </div>
+  )
+}
+
+export default spinner
